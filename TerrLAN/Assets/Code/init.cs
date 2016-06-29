4dde7f65aa024aad45629f65dc8f2a4facf5770a
Chad Frederick <freedomrock1@gmail.com>
2016-06-28 22:31:16 -0400

    Scene Init

---
+++ TerrLAN/Assets/Code/init.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class init : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    // load building
+
+
+        // load network 
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
