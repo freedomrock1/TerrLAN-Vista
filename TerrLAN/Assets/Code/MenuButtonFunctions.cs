fc367b561b4f3d4187d912595f6816332c88c6b5
Avery Snyder <Madrauk@yahoo.com>
2016-06-30 17:09:59 -0400

    GUI Progress
    Scans for files and makes a button for each, all buttons currently
    inoperable.

---
+++ TerrLAN/Assets/Code/MenuButtonFunctions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadResource(string path)
+    {
+        GameObject temp = Resources.Load(path) as GameObject;
+        this.gameObject.transform.GetComponentInParent<GUIcontroller>().PlacingObject = temp;
+        this.gameObject.transform.GetComponentInParent<GUIcontroller>().ToggleGUI();
+    }
+}
