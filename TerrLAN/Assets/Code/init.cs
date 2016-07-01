c9115411dd65a4267746b8224f430c69625f7c30
Harrison John Hopkins <hguyhopkins@gmail.com>
2016-06-30 20:42:36 -0400

    Added Floorplan
    3D Floorplan loads in at run-time.

--- TerrLAN/Assets/Code/init.cs
+++ TerrLAN/Assets/Code/init.cs
@@ -6,8 +6,8 @@ public class init : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         // load building
-        GameObject instance = Instantiate(Resources.Load("skyscraperCollider", typeof(GameObject))) as GameObject;
-        instance.transform.position = new Vector3(transform.position.x , 0.4f, transform.position.z );
+        GameObject instance = Instantiate(Resources.Load("Building view", typeof(GameObject))) as GameObject;
+        instance.transform.position = new Vector3(transform.position.x , 0.15f, transform.position.z );
         // load network 
 
 
