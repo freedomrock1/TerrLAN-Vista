6b571761827f778b4b16d2787c6819faae1a3739
Avery Snyder <Madrauk@yahoo.com>
2016-07-06 16:57:27 -0400

    Asset bundles can now be loaded. PS, FPS controller can't move. Not my fault, not sure why.

--- TerrLAN/Assets/Code/GUIcontroller.cs
+++ TerrLAN/Assets/Code/GUIcontroller.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityStandardAssets.Characters.FirstPerson;
 using System.IO;
 using UnityEngine.UI;
+using UnityEditor;
 
 
 public class GUIcontroller : MonoBehaviour {//attach to canvas of GUI
@@ -18,28 +19,7 @@ public class GUIcontroller : MonoBehaviour {//attach to canvas of GUI
 	// Use this for initialization
 	void Start () {
         Cursor.lockState = CursorLockMode.Locked;
-        string[] fileInfoStrings = Directory.GetFiles(Application.dataPath + "/Resources");
-        FileInfo[] fileInfo = new FileInfo[fileInfoStrings.Length];
-        for(int i=0;i<fileInfoStrings.Length;i++)
-        {
-            fileInfo[i] = new FileInfo(fileInfoStrings[i]);
-        }
-        buttonList = new GameObject[fileInfo.Length];
-        for(int p=0;p<fileInfo.Length;p++)
-        {
-            if(fileInfo[p].Extension != ".meta")
-            {
-                buttonList[p] = Instantiate(button);
-                buttonList[p].transform.parent = scrollView.transform;
-                buttonList[p].GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -75 - buttonList[p].GetComponent<RectTransform>().sizeDelta.y * p);
-                buttonList[p].GetComponentInChildren<Text>().text = fileInfo[p].Name;
-                //string temp = fileInfo[p].Name.Replace(fileInfo[p].Extension, null);
-                int temp = p;
-                buttonList[p].GetComponent<Button>().onClick.AddListener(delegate { LoadResource(temp, fileInfo); });
-                buttonList[p].gameObject.SetActive(!buttonList[p].gameObject.active);
-            }
-        }
-        scrollView.GetComponent<RectTransform>().sizeDelta = new Vector2(scrollView.GetComponent<RectTransform>().sizeDelta.x, (buttonList.Length+5) * button.GetComponent<RectTransform>().sizeDelta.y);
+        RefreshFileList();
 	}
 	
 	// Update is called once per frame
@@ -65,24 +45,15 @@ public class GUIcontroller : MonoBehaviour {//attach to canvas of GUI
 
     void LoadResource(int index, FileInfo[] fileInfo)
     {
-        if(fileInfo[index].Extension == ".unitypackage")
+        if(fileInfo[index].Extension == "")
         {
-            Debug.Log("file:///" + fileInfo[index].DirectoryName.Replace("\\","/") + "/" + fileInfo[index].Name);
-            //AssetBundle temp = AssetBundle.LoadFromFile(fileInfo[index].DirectoryName + "/" + fileInfo[index].Name);
-            WWW www = new WWW("file:///" + fileInfo[index].DirectoryName.Replace("\\", "/") + "/" + fileInfo[index].Name);
-            AssetBundle temp = www.assetBundle;
-            Object[] objList = www.assetBundle.LoadAllAssets<Object>();
-            GameObject[] gameObjectList = new GameObject[objList.Length];
-            for(int i=0;i<objList.Length;i++)
+            string path = fileInfo[index].DirectoryName + "/" + fileInfo[index].Name;
+            AssetBundle temp = AssetBundle.LoadFromFile(path);
+            Object[] objList;
+            objList = temp.LoadAllAssets();
+            foreach (GameObject g in objList)
             {
-                gameObjectList[i] = Instantiate(objList[i]) as GameObject;
-            }
-            foreach(GameObject g in gameObjectList)
-            {
-                if(g.tag == "building")
-                {
-                    placingObject = Instantiate(g);
-                }
+                placingObject = Instantiate(g);
             }
         }
         else
@@ -124,5 +95,33 @@ public class GUIcontroller : MonoBehaviour {//attach to canvas of GUI
         {
             ToggleActiveGUI(this.gameObject.transform.GetChild(i).gameObject);
         }
+    }
+
+    void RefreshFileList()
+    {
+        string[] fileInfoStrings = Directory.GetFiles(Application.dataPath + "/Resources");
+        FileInfo[] fileInfo = new FileInfo[fileInfoStrings.Length];
+        for (int i = 0; i < fileInfoStrings.Length; i++)
+        {
+            fileInfo[i] = new FileInfo(fileInfoStrings[i]);
+        }
+        buttonList = new GameObject[fileInfo.Length];
+        int actualLength = -1;
+        for (int p = 0; p < fileInfo.Length; p++)
+        {
+            if (fileInfo[p].Extension != ".meta" && fileInfo[p].Extension != ".manifest" && fileInfo[p].Extension != ".unitypackage")
+            {
+                actualLength++;
+                buttonList[p] = Instantiate(button);
+                buttonList[p].transform.parent = scrollView.transform;
+                buttonList[p].GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -75 - buttonList[p].GetComponent<RectTransform>().sizeDelta.y * actualLength);
+                buttonList[p].GetComponentInChildren<Text>().text = fileInfo[p].Name;
+                //string temp = fileInfo[p].Name.Replace(fileInfo[p].Extension, null);
+                int temp = p;
+                buttonList[p].GetComponent<Button>().onClick.AddListener(delegate { LoadResource(temp, fileInfo); });
+                buttonList[p].gameObject.SetActive(!buttonList[p].gameObject.active);
+            }
+        }
+        scrollView.GetComponent<RectTransform>().sizeDelta = new Vector2(scrollView.GetComponent<RectTransform>().sizeDelta.x, (actualLength + 5) * button.GetComponent<RectTransform>().sizeDelta.y);
     }
 }
