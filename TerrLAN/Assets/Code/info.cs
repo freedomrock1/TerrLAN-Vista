cc87745c2bf2e5491f156e1b42b88d8891195bbd
Avery Snyder <Madrauk@yahoo.com>
2016-07-22 12:36:06 -0400

    Implemented persistent data object
    Starts from scene "loading", load in city and building name through
    inspector and see their values come up in the scene change to
    prelan-vista

---
+++ TerrLAN/Assets/Code/info.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class info : MonoBehaviour {
+
+    public string city;
+    public string building;
+    public string nextScene;
+
+	// Use this for initialization
+	void Start () {
+        Application.LoadLevel(nextScene);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ TerrLAN/Assets/Code/infoTester.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class infoTester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Text text = this.gameObject.GetComponent<Text>();
+        GameObject info = GameObject.Find("info");
+        text.text = "City: " + info.GetComponent<info>().city + "\n" + "Building: " + info.GetComponent<info>().building + "\nClick anywhere to proceed to lan-vista";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            Application.LoadLevel("lan-vista");
+        }
+	}
+}
